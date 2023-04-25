f5f40e04a3b5f0bb4dcf896f4857e93f6880f7b6
sasha <usakolumb@gmail.com>
2023-04-25 13:06:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+hello my name Aleksandr 
+what your name ?
---
+++ name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << "write name" << endl;
+	string name = c;
+	cin >> c;
+	cout << "hello" << 'c' << endl;
+}
